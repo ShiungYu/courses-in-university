170f12e87bca4d43bb7a0201b4803fd5d9e80882
ShiungYu <104500800+ShiungYu@users.noreply.github.com>
2022-05-26 22:51:18 +0800

    Add files via upload

---
+++ java programming/exercise0/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass
+{
+// main method begins execution of Java application
+public static void main( String args[] )
+{
+System.out.println( "?™æ˜¯?‘ç?ç¬¬ä??¯Javaç¨‹å?!\nå­¸è?ï¼š\"00957118\"\nå§“å?ï¼š\"?¡ç?å®‡\"" );
+System.out.println( "" );
+} // end method main
+} // end class FirstClass
